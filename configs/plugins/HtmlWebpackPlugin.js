fcec52f188d0fa581b5e03fd36ef35b72f4bf2b1
Veronika Zayarnaya <veronika.zayarnaya@yandex.ru>
2025-01-20 15:29:18 +0300

    feat: add favicon

--- configs/plugins/HtmlWebpackPlugin.js
+++ configs/plugins/HtmlWebpackPlugin.js
@@ -3,5 +3,6 @@ const path = require('path');
 
 module.exports = new HtmlWebpackPlugin({
 	template: path.join(__dirname, './../../index.html'),
+	favicon: path.join(__dirname, './../../favicon.svg'),
 	filename: 'index.html',
 });
